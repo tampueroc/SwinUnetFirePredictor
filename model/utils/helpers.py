62fdbfb420c3b0975b9946d32638a83d9da467f0
Tomás Ampuero <tom.ampuero.c@gmail.com>
2025-01-22 18:56:13 -0600

    fix: cast displacement as tuple

--- model/utils/helpers.py
+++ model/utils/helpers.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class CyclicShift3D(nn.Module):
     def __init__(self, displacement):
         super().__init__()
-        self.displacement = displacement
+        self.displacement = tuple(displacement)
 
     def forward(self, x):
         return torch.roll(x, shifts=self.displacement, dims=(1, 2, 3))
